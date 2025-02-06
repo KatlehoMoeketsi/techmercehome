a1d2e432f1adf8649ddb744bbf0ddecf02d3bb73
KatlehoMoeketsi <kaydawg718@gmail.com>
2025-02-06 14:37:51 +0200

    Delete function added with call to tkinter window for delete. Removed pack positioning and used grid instead.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 
 import sqlite3
 import tkinter as tk
+from idlelib.debugger_r import restart_subprocess_debugger
 from tkinter import messagebox
 
 #Setting up the Database
@@ -40,24 +41,68 @@ def add_client():
     phone_entry.delete(0, tk.END)
 
 
+#Delete Client Window
+def open_delete_window():
+    delete_window = tk.Toplevel(root)
+    delete_window.title("Delete Client")
+    delete_window.geometry("300x150")
+
+    tk.Label(delete_window, text="Client ID to Delete: ").pack()
+    id_entry = tk.Entry(delete_window)
+    id_entry.pack()
+
+
+    def delete_client():
+        # Get the id from the input
+        client_id = id_entry.get()
+        if not client_id.isdigit():
+            messagebox.showerror("Error", "Please enter a valid numeric ID")
+            return
+
+        conn = sqlite3.connect("clients.db")
+        cursor = conn.cursor()
+
+        # check if client exists
+        cursor.execute("SELECT * FROM clients WHERE id=?", (client_id))
+        result = cursor.fetchone();
+
+        if result:
+            confirm = messagebox.askyesno("Confirm", f"Are you sure you want to delete Client ID {client_id}?")
+            if confirm:
+                # Important code below: Executes the SQL command on the database
+                cursor.execute("DELETE FROM clients WHERE (id) = (?)", (client_id,))
+                conn.commit()
+                messagebox.showinfo("Success", "Client Deleted Successfully")
+            else:
+                messagebox.showinfo("Canceled", "Deletion Canceled")
+        else:
+            messagebox.showinfo("Error", "Client ID Not Found")
+        conn.close()
+        id_entry.delete(0, tk.END)  # Clear the input
+
+    tk.Button(delete_window, text="Delete", command=delete_client).pack()
+
 #GUI Setup
 root = tk.Tk()
 root.title('Client Database')
 root.geometry("400x300")
 
-tk.Label(root, text="Name:").pack()
+tk.Label(root, text="Name:").grid(row=0, column=1)
 name_entry = tk.Entry(root)
-name_entry.pack()
+name_entry.grid(row=1, column=1)
 
-tk.Label(root, text="Email:").pack()
+tk.Label(root, text="Email:").grid(row=2, column=1)
 email_entry = tk.Entry(root)
-email_entry.pack()
+email_entry.grid(row=3, column=1)
 
-tk.Label(root, text="Phone:").pack()
+tk.Label(root, text="Phone:").grid(row=4, column=1)
 phone_entry = tk.Entry(root)
-phone_entry.pack()
+phone_entry.grid(row=5, column=1)
 
-tk.Button(root,text="Add Client", ).pack
+
+
+tk.Button(root,text="Add Client", command=add_client).grid(row=6, column=1, padx= 5, pady= 8)
+tk.Button(root,text="Delete Client", command=open_delete_window).grid(row=7, column=1, padx= 5, pady= 8)
 
 init_db()
 
