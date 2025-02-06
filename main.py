5cffd2bdf0aab29f995b80dbc1d6f74e65da8819
KatlehoMoeketsi <kaydawg718@gmail.com>
2025-02-06 12:31:27 +0200

    Initial Commit

---
+++ main.py
@@ -0,0 +1,5 @@
+#This project will serve as a database for local SMMEs and potential client/client information for Techmerce.
+#It will use tkInter for GUI and will initially use a simple local SQL database, then we will move towards firebase
+#for cloud services.
+
+print("This is techmerce")
