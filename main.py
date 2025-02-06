6cb6fc81073e79c1ebe6e75c9578e0a75554783e
KatlehoMoeketsi <kaydawg718@gmail.com>
2025-02-06 12:57:13 +0200

    Initial Code created.

--- main.py
+++ main.py
@@ -2,4 +2,44 @@
 #It will use tkInter for GUI and will initially use a simple local SQL database, then we will move towards firebase
 #for cloud services.
 
-print("This is techmerce")
+import sqlite3
+import tkinter as tk
+from tkinter import messagebox
+
+#Setting up the Database
+def init_db():
+    conn = sqlite3.connect("clients.db")
+    cursor = conn.cursor()
+    cursor.execute()
+
+    conn.commit()
+    conn.close()
+
+#Add Client
+
+
+#GUI Setup
+root = tk.Tk()
+root.title('Client Database')
+root.geometry("400x300")
+
+tk.Label(root, text="Name:").pack()
+name_entry = tk.Entry(root)
+name_entry.pack()
+
+tk.Label(root, text="Email:").pack()
+email_entry = tk.Entry(root)
+email_entry.pack()
+
+tk.Label(root, text="Phone:").pack()
+phone_entry = tk.Entry(root)
+phone_entry.pack()
+
+tk.Button(root,text="Add Client").pack
+
+init_db()
+
+root.mainloop()
+
+
+
